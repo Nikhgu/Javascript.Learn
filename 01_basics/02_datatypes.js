fe3b732a51e3a0e47ce3a4fb657e15185025e805
Nikhgu <97823097+Nikhgu@users.noreply.github.com>
2025-08-05 12:54:31 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,37 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert( 3+3) // we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3)//code readability should be high  
+
+ console.log("Nikhil Gupta")
+
+ let name = "Nikhil"
+ let age = 25
+ let isLoggedIn = false
+
+//primative DataTypes
+ // number => 2 to power 53
+ //bigint
+ //string => ""
+ //boolean => true/false
+ //null => standalone value 
+ //undefined
+ //symbol => is used for uniqueness mostly used in react and figma tools 
+
+
+ //object 
+ 
+ console.log(typeof "Nikhil");
+  console.log(typeof age);
+   console.log(typeof null);// null type is object
+    console.log(typeof undefined);//undefined type is undefined 
+
+    
+ 
+
+
+
+
