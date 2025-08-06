e68ef8d3909321e84ea476218344d52eefae0410
Nikhgu <97823097+Nikhgu@users.noreply.github.com>
2025-08-06 15:15:56 +0000

    [04_comparison]

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+//most of this things should be avoid null and undefined 
+// console.log(undefined == 0);
+// console.log(undefined == 1);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === not only checks the value but it also checks the datatype
+
+console.log("2" == 2);
+console.log("2" === 2);
