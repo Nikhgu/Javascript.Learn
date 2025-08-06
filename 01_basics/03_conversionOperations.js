e7734bcd68c4ab985816777071bb95079e5cab65
Nikhgu <97823097+Nikhgu@users.noreply.github.com>
2025-08-06 12:32:12 +0000

    01_basics/03_conversionOperations.js  #

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,41 @@
+let score = false
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+
+//console.log( valueInNumber)
+
+let value = null
+
+//console.log(typeof value)
+//console.log( value)
+let valueNumber = Number(value)
+//console.log(typeof valueNumber)
+//console.log( valueNumber)
+
+
+// "33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Nikhil"
+
+let booleanIsLogggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLogggedIn);
+
+
+// 1 => true 0 => false
+// "" => false "Nikhil" => true 
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
