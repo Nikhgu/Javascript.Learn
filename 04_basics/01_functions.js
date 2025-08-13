1b07cb14e778249b043f4ff8dd9186f4751055f8
Nikhgu <97823097+Nikhgu@users.noreply.github.com>
2025-08-13 10:58:52 +0000

    []

--- 04_basics/01_functions.js
+++ 04_basics/01_functions.js
@@ -32,18 +32,48 @@ console.log("L");
 
 function loginUserMessage(username = "Sam"){
     if(!username) {
-       console.log("Please provide a username");
+      // console.log("Please provide a username");
        return 
     }
     return `${username} just logged in!` 
 }
 //console.log(loginUserMessage("Nikhil"))
-console.log(loginUserMessage("hitesh"));
+//console.log(loginUserMessage("hitesh"));
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+//console.log(calculateCartPrice(200, 400, 500, 2000)); 
+
+//...num1 is a rest parameter(operator)
+//it allows us to pass an indefinite number of arguments to the function
+//it will return an array of all the arguments passed to the function
+
+const user = {
+    username: "Nikhil",
+    price: 199,
+}
+
+   function handleObject(anyObject) {
+  //console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+  
+   }
+
+// handleObject(user);
+handleObject({
+    username: "Sam",
+    price: 399,
+})
 
 
+const myNewArray = [200, 400, 100, 600]
 
+function returnSecondValue(getArray){
+  return getArray[1]
 
-
+}
+//console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000])); //200
 
 
 
