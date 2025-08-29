c0ff96059356ad39ec20589c84a1755514dea277
Nikhgu <97823097+Nikhgu@users.noreply.github.com>
2025-08-29 09:22:26 +0000

    []

--- 05_control_flow/one.js
+++ 05_control_flow/one.js
@@ -37,17 +37,17 @@
 // }  
 
 
-const userLoggedIn = true 
-const debitCard = true 
-const loggedInFromGoogle = false 
-const loggedInFromEmail = true
+// const userLoggedIn = true 
+// const debitCard = true 
+// const loggedInFromGoogle = false 
+// const loggedInFromEmail = true
 
-if(userLoggedIn && debitCard  ){
-    console.log("Allow to buy courses");
-}
-if(loggedInFromGoogle || loggedInFromEmail){
-    console.log("User logged in"); 
-}
+// if(userLoggedIn && debitCard  ){
+//     console.log("Allow to buy courses");
+// }
+// if(loggedInFromGoogle || loggedInFromEmail){
+//     console.log("User logged in"); 
+// }
 
 
 
