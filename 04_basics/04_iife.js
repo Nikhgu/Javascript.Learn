08b50403c7f6985e96ef2d2b51fc2f3336c2caac
Nikhgu <97823097+Nikhgu@users.noreply.github.com>
2025-08-29 01:03:41 +0000

    [Javascript Execution Context]

--- 04_basics/04_iife.js
+++ 04_basics/04_iife.js
@@ -2,13 +2,15 @@
 //IIFE is used when you don't want to access the global pollution(polute(variable)) the child element to stop using it so we use iife
 //  We want to execute the function immeditely  and the cause of global pollution(polute) which polute the child function to remove this problem we use iife 
 (function chai() {
+    // named IIFE
     console.log("DB CONNECTED");
 })();
 
 //(function definition) (execution call)
 
-( function aurcode()  {
-    console.log(`DB CONNECTED TWO`);
+((name) => {
+    //unamed IIFE
+    console.log(`DB CONNECTED TWO ${name}`);
     
-}  )()
+})('hitesh');
 
---
+++ execution.js
@@ -0,0 +1,34 @@
+// Javascript Exectution Context 
+
+//Execution context means how javascript files are run and execute.
+//Files are run into two phases
+// 1)memory Creation Phase/creation phase in this phase we useally allocate a memory for variable or declare thing and we don't execute it.  
+//2)Exection phase in this phase 
+
+let val1 = 10
+let val2 = 5
+function addNum(num1, num2) { 
+    let total = num1 + num2
+    return total
+}
+let result1 = addNum(val1, val2);
+let result2 = addNum(10, 2)
+//1) Global Execution(global environment) code run firstly in global execution then allocte to (this)  
+//2)Memory Creation Phase in this we gather all the variables store in a memory 
+// Ist Cycle 
+// val1=> undefined
+// val2 => undefined
+// addNum => definition 
+// result1=>undefined
+//result2=>undefined
+// 3)  Execution Phase
+//val1=> 10
+//val2 => 5
+//addNum =>another execution is created for result1 
+// every time the function is executed every time new executional context(new variable environment + execution thread ) 
+// new variable environment + execution thread 
+//memory phase 
+// val1=> undefined
+//val2 => undefined
+//total=> undefined
+//execution context then processing starts 
