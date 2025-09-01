1a9bce385d7ee6852521a779e8998b68aac5200c
Nikhgu <97823097+Nikhgu@users.noreply.github.com>
2025-09-01 15:40:48 +0000

    execution

--- execution.js
+++ execution.js
@@ -1,11 +1,19 @@
 // Javascript Exectution Context 
+// 
+
+//Global Execution Context
+//Function Execution Context
+//Eval Execution Context in mongoose is a property for global EC
 
 //Execution context means how javascript files are run and execute.
+//Firstly we give code to javascript the global execution is created everytime and global execution context is stored to (this variable ) where it is stored 
+// ** For Browser the execution context is window
+
+
 //Files are run into two phases
 // 1)memory Creation Phase/creation phase in this phase we useally allocate a memory for variable or declare thing and we don't execute it.  
 //2)Exection phase in this phase 
 
-let val1 = 10
 let val1 = 10
 let val2 = 5
 function addNum(num1, num2) { 
@@ -26,10 +34,40 @@ let result2 = addNum(10, 2)
 //val1=> 10
 //val2 => 5
 //addNum =>another execution is created for result1 
-// every time the function is executed every time new executional context(new variable environment + execution thread ) 
+//(// every time the function is executed every time new executional context(new variable environment + execution thread ) 
 // new variable environment + execution thread 
 //memory phase 
 // val1=> undefined
 //val2 => undefined
 //total=> undefined
-//execution context then processing starts 
+//execution context then processing starts then execute mathmatical content and data manipulate 
+//num1=>10
+//num2=>2
+//total=>12
+//total is return to global executional context 
+//now the new executional context will be deleted 
+//)
+
+// result1=>15
+//result2 =>undefined
+//addNum => another execution is created for result2
+//(// every time the function is executed every time new executional context(new variable environment + execution thread ) 
+// new variable environment + execution thread 
+//memory phase 
+// val1=> undefined
+//val2 => undefined
+//total=> undefined
+//execution context then processing starts then execute mathmatical content and data manipulate 
+//num1=>10
+//num2=>2
+//total=>12
+//total is return to global executional context 
+//now the new executional context will be deleted 
+//)
+
+
+
+//Call Stack 
+// Firstly Global execution is pushed into stack
+// Then the function will pushed onto  the stack(function method  one()) is executed is poped onto the stack
+//lifo execution
